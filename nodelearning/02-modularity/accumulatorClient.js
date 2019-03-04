5be57a3370335304a11b7309d0a35b78e86af7dd
Harish <harishtm77@gmail.com>
2019-03-04 11:00:18 +0530

    node modularity

--- nodelearning/02-modularity/accumulatorClient.js
+++ nodelearning/02-modularity/accumulatorClient.js
@@ -1,1 +1,8 @@
-var acc = require
+var acc = require('./accumulator.js')();
+
+acc.add(100);
+acc.substract(50);
+acc.multiply(2);
+acc.divide(20)
+
+console.log("Result for the above operation is : ", acc.getresult())
