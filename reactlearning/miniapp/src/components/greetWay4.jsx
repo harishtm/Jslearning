c6d140ba863f78cc01ea71ad5c523305a9a395de
Harish <harishtm77@gmail.com>
2019-03-26 15:53:51 +0530

    another example 4

---
+++ reactlearning/miniapp/src/components/greetWay4.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import GreetWay2 from './greetWay2';
+
+
+class GreetWay4 extends Component {
+    render () {
+        return (
+            <Greeter message="Welcome to react"></Greeter>
+        )
+    }
+}
+
+const Greeter = (props) => <h4>{ props.message }</h4>;
+
+export default GreetWay4;
--- reactlearning/miniapp/src/index.js
+++ reactlearning/miniapp/src/index.js
@@ -6,11 +6,13 @@ import * as serviceWorker from './serviceWorker';
 import SimpleApp from './components/greetWay1'
 import GreetWay2 from './components/greetWay2'
 import GreetWay3 from './components/greetWay3'
+import GreetWay4 from './components/greetWay4'
 
 
 ReactDOM.render(<SimpleApp />, document.getElementById('way1'));
 ReactDOM.render(<GreetWay2 />, document.getElementById('way2'));
 ReactDOM.render(<GreetWay3 />, document.getElementById('way3'));
+ReactDOM.render(<GreetWay4 />, document.getElementById('way4'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
