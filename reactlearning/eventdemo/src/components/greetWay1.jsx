72a6f86cc4a3c41144efb2e50764f626a001440b
Harish <harishtm77@gmail.com>
2019-03-28 10:23:02 +0530

    Event handling

--- reactlearning/eventdemo/src/components/greetWay1.jsx
+++ reactlearning/eventdemo/src/components/greetWay1.jsx
@@ -20,12 +20,13 @@ class GreeterWay1 extends Component {
         return (
             <div>
                 <Greeting message={msg} isShow={ this.state.isShow }></Greeting>
+                <button onClick={this.toggleShow} type="button">Toggle Show</button>
             </div>
         )
     }
 }
 
 
-const Greeting = ({message, isShow}) => isShow ? <h4>{message}</h4> : "No message to display";
+const Greeting = ({message, isShow}) => isShow ? <h4>{message}</h4> : null;
 
 export default GreeterWay1;
