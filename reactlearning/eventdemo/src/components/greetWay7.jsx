fe82da0818f4aa599ec86868611738fb263657ec
Harish <harishtm77@gmail.com>
2019-04-03 15:35:13 +0530

    handling default props

--- reactlearning/eventdemo/src/components/greetWay7.jsx
+++ reactlearning/eventdemo/src/components/greetWay7.jsx
@@ -7,13 +7,16 @@ class GreeterWay7 extends Component {
         const params = {description: 'Your New Component Library'}
         return (
             <div>
-                <Greeting {...params}></Greeting>
+                <GreetingWay1 {...params}></GreetingWay1>
+                <GreetingWay2 {...params}></GreetingWay2>
+                <GreetingWay3 {...params}></GreetingWay3>
+                <GreetingWay4></GreetingWay4>
             </div>
         )
     }
 }
 
-const Greeting = ({subject, description}) => {
+const GreetingWay1 = ({subject, description}) => {
     // flower braces as return is used
     subject = subject || 'Default Text goes here....';
     return(
@@ -23,6 +26,34 @@ const Greeting = ({subject, description}) => {
         </div>
     )
     
+}
+
+const GreetingWay2 = ({subject, description}) => (
+    <div>
+        <Title title={`Welcome to ${subject || 'Default subject'}`}></Title>
+        <Description desc={description}></Description>
+    </div>
+)
+
+const GreetingWay3 = ({subject = 'Default Text', description}) => {
+    return (
+        <div>
+            <Title title={`Welcome to ${subject}`}></Title>
+            <Description desc={description}></Description>
+        </div>
+    )
+}
+
+const GreetingWay4 = ({subject, description}) => (
+    <div>
+        <Title title={`Welcome to ${subject || 'Default subject'}`}></Title>
+        <Description desc={description}></Description>
+    </div>
+)
+
+GreetingWay4.defaultProps = {
+    subject: 'Default Msg',
+    description: 'Default prop Componet'
 }
 
 const Title = ({title}) => (
