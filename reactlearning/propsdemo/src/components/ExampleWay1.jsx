3869e8c0ef6a9af3af242589a6f9deb7428a1188
Harish <harishtm77@gmail.com>
2019-04-03 16:22:50 +0530

    reacts children prop

---
+++ reactlearning/propsdemo/src/components/ExampleWay1.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+// React's children prop
+// Note: argument should be "children" only to get the contents
+
+class ExampleWay1 extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShow: true
+        };
+    }
+
+    toggleShow = () => {
+        this.setState(state => ({isShow: !state.isShow}))
+    }
+
+    render() {
+        const msg = 'React Props example'
+        return(
+            <div>
+                <Greeting message={msg} isShow={ this.state.isShow }></Greeting>
+                <Button onClick={this.toggleShow}>Toggle HTML Text</Button>
+            </div>
+        )
+    }
+}
+
+const Greeting = ({message, isShow}) => isShow ? <h4>{message}</h4> : null;
+
+const Button = ({onClick, children}) => (
+    // parameter "children" should be exact
+    <button onClick={onClick} type="button">
+        {children}
+    </button>
+);
+
+export default ExampleWay1;
