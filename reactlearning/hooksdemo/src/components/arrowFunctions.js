e8e0faaf0954f305200baeefe4828e181e212ec6
Harish <harishtm77@gmail.com>
2019-04-12 10:31:09 +0530

    understanding arrowFunctions

--- reactlearning/hooksdemo/src/components/arrowFunctions.js
+++ reactlearning/hooksdemo/src/components/arrowFunctions.js
@@ -38,3 +38,47 @@ var docLogES5 = function docLog() {
 // ES6
 
 var docLog = () => console.log(document)
+
+// Object literal syntax
+
+// Arrow functions, like function expressions, can be used to return an object literal expression.
+// The only caveat is that the body needs to be wrapped in parentheses,
+// in order to distinguish between a block and an object (both of which use curly brackets).
+
+var setNameIdsES5 = function setnameIds(id, name) {
+    return {
+        id: id,
+        name: name
+    }
+}
+
+var setNameIdsES6 = () => ({id:id, name:name});
+
+// Use case for arrow functions
+
+// One common use case for arrow functions is array manipulation and the like.
+// It’s common that you’ll need to map or reduce an array
+
+const smartPhones = [
+    { name:'iphone', price:649 },
+    { name:'Galaxy S6', price:576 },
+    { name:'Galaxy Note 5', price:489 }
+  ];
+
+// ES5
+
+var price = smartPhones.map(function(smartPhones){
+    return smartPhones.price;
+})
+
+// ES6
+
+const prices = smartPhones.map(smartPhone => smartPhone.price)
+
+// Here’s another example using the array filter method:
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+var divisibleby3ES5 = array.filter(function(val){
+    return val % 3 == 0;
+})
